305c0ef435b9248b9e0e1c78df2c26fbb4b6ec2c
gal kahana <gal.kahana@hotmail.com>
2013-04-09 09:31:06 +0300

    moved close event handling on writestream as it should

--- tests/BasicModification.js
+++ tests/BasicModification.js
@@ -21,22 +21,22 @@ function testInPlaceFileModification(inFileName)
 {
     var fs = require('fs');
     
-    fs.createReadStream('../deps/TestMaterials/' + inFileName + '.pdf').
-        pipe(fs.createWriteStream('./Output/InPlaceModified' + inFileName + '.pdf')).on('close', function()
-                                                                                        {
-                                                                                        var pdfWriter = hummus.createWriterToModify('./Output/InPlaceModified' + inFileName + '.pdf');
-                                                                                        var page = pdfWriter.createPage(0,0,595,842);
-                                                                                        
-                                                                                        pdfWriter.startPageContentContext(page).BT()
-                                                                                        .k(0,0,0,1)
-                                                                                        .Tf(pdfWriter.getFontForFile('../deps/TestMaterials/fonts/Courier.dfont',0),1)
-                                                                                        .Tm(30,0,0,30,78.4252,662.8997)
-                                                                                        .Tj('about')
-                                                                                        .ET();
-                                                                                        
-                                                                                        pdfWriter.writePage(page);
-                                                                                        pdfWriter.end();
-                                                                                        });
+    var ws = fs.createWriteStream('./Output/InPlaceModified' + inFileName + '.pdf');
+    ws.on('close', function()
+           {
+            var pdfWriter = hummus.createWriterToModify('./Output/InPlaceModified' + inFileName + '.pdf');
+            var page = pdfWriter.createPage(0,0,595,842);
+           
+            pdfWriter.startPageContentContext(page).BT()
+                                                    .k(0,0,0,1)
+                                                    .Tf(pdfWriter.getFontForFile('../deps/TestMaterials/fonts/Courier.dfont',0),1)
+                                                    .Tm(30,0,0,30,78.4252,662.8997)
+                                                    .Tj('about')
+                                                    .ET();
+           pdfWriter.writePage(page);
+           pdfWriter.end();
+           });
+    fs.createReadStream('../deps/TestMaterials/' + inFileName + '.pdf').pipe(ws);
 }
 
 testBasicFileModification('BasicTIFFImagesTest');
