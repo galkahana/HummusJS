f8a87181d3f037917520cf50d9464cc97efbdcd3
Brian Reavis <brian@thirdroute.com>
2015-10-22 14:24:22 -0600

    Cleanup: explicitly defined files to publish (saves ~27mb) + "test" script

--- runTests.js
+++ runTests.js
@@ -2,24 +2,22 @@ var fs = require('fs');
 var cp = require('child_process');
 
 if (!String.prototype.endsWith) {
-    Object.defineProperty(String.prototype, 'endsWith', {
-                          enumerable: false,
-                          configurable: false,
-                          writable: false,
-                          value: function (searchString, position) {
-                          position = position || this.length;
-                          position = position - searchString.length;
-                          return this.lastIndexOf(searchString) === position;
-                          }
-                          });
+	Object.defineProperty(String.prototype, 'endsWith', {
+		enumerable: false,
+		configurable: false,
+		writable: false,
+		value: function (searchString, position) {
+			position = position || this.length;
+			position = position - searchString.length;
+			return this.lastIndexOf(searchString) === position;
+		}
+	});
 }
 
 var files = fs.readdirSync(__dirname + '/tests');
-files.forEach(
-        function(element, index, array)
-              {
-                    var aString = element.toString();
-                    if(aString.endsWith('.js'))
-                        cp.spawn('node',[aString], {cwd:__dirname + '/tests', stdio: 'inherit' });
-              }
-    );
+files.forEach(function(element, index, array) {
+	var aString = element.toString();
+	if (aString.endsWith('.js')) {
+		cp.spawn('node',[aString], {cwd:__dirname + '/tests', stdio: 'inherit' });
+	}
+});
