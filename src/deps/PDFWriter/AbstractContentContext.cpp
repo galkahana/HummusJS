cd0ad334a1180b846520dc5295e12e74d9c4c138
galkahana <gal.kahana@hotmail.com>
2016-08-29 00:29:03 +0300

    oh what a stupid bug that killed proper unicode encoding

--- src/deps/PDFWriter/AbstractContentContext.cpp
+++ src/deps/PDFWriter/AbstractContentContext.cpp
@@ -1124,8 +1124,12 @@ EStatusCode AbstractContentContext::WriteTextCommandWithDirectGlyphSelection(con
 	{
 		for(;it!= encodedCharactersList.end();++it)
 		{
-			SAFE_SPRINTF_2(formattingBuffer,5,"%02x%02x",((*it)>>8) & 0x00ff,(*it) & 0x00ff);
-			stringStream.Write((const Byte*)formattingBuffer,4);
+			formattingBuffer[0] = ((*it) >> 8) & 0x00ff;
+			stringStream.Write((const Byte*)formattingBuffer, 1);
+			formattingBuffer[0] = (*it) & 0x00ff;
+			stringStream.Write((const Byte*)formattingBuffer, 1);
+			//SAFE_SPRINTF_2(formattingBuffer,5,"%02x%02x",((*it)>>8) & 0x00ff,(*it) & 0x00ff);
+			//stringStream.Write((const Byte*)formattingBuffer,4);
 		}
 		inTextCommand->WriteHexStringCommand(stringStream.ToString());
 	}
