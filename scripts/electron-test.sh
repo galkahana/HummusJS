bc7b4f6eed7df658a6a6c5bb58e8800da5d5f6f2
galkahana <gal.kahana@hotmail.com>
2019-05-04 21:22:54 +0300

    use electron mocha and electron when testing

---
+++ scripts/electron-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+npm install -g electron@$ELECTRON_VERSION
+if [ "$MOCHA_RUNTIME_VERSION" = "" ]; then 
+    npm install -g electron-mocha
+else
+    npm install -g electron-mocha@MOCHA_RUNTIME_VERSION
+fi
+electron-mocha -R tap ./tests/*.js --timeout 15000
