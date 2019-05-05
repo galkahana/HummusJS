720526cad50ce450e682d4668d2fa48ff93d43d5
galkahana <gal.kahana@hotmail.com>
2019-05-05 22:40:40 +0300

    lets try whats on em site

--- scripts/electron-test.sh
+++ scripts/electron-test.sh
@@ -9,8 +9,7 @@ else
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
-    export DISPLAY=':99.0'
-    Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+    export DISPLAY=:99.0; sh -e /etc/init.d/xvfb start
 fi
 
 electron-mocha -R tap ./tests/*.js --timeout 15000
