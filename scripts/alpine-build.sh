678c93fab761ffba6d6bb47d44738ca8297af909
galkahana <gal.kahana@hotmail.com>
2023-04-12 15:20:12 +0300

    main branch restriction, readme update and dropping old build systems descriptors

--- scripts/alpine-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run -v ${PWD}:/build -i node:${NODE_NVM_VERSION}-alpine /bin/sh <<-EOF
-apk add --no-cache make gcc jq g++ python
-cd /build
-EXTRA_NODE_PRE_GYP_FLAGS="--target_libc=musl" npm install --build-from-source --unsafe-perm
-EOF
-sudo chown -R travis ${PWD}
--- scripts/alpine-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run -v ${PWD}:/build -i node:${NODE_NVM_VERSION}-alpine /bin/sh <<-EOF
-cd /build
-$*
-npm test
-EOF
-sudo chown -R travis ${PWD}
